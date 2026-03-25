902760acfb7dbfedb4bbfc9085ca46ae51f13234
emco7h-droid <emco7h@gmail.com>
2026-03-25 17:35:21 +0100

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-export default function Home() {
-  const r = useRouter()
-  useEffect(() => { r.push('/dashboard') }, [])
-  return null
-}
