ac60ad274240fed37a705a9c7bf597194e1a3c71
emco7h-droid <emco7h@gmail.com>
2026-04-02 10:27:09 +0200

    Delete app directory

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-import './globals.css'
-export const metadata = { title: 'Growth Hub', description: 'Agency OS for Growth Operators' }
-export default function RootLayout({ children }) {
-  return (
-    <html lang="de">
-      <body>{children}</body>
-    </html>
-  )
-}
